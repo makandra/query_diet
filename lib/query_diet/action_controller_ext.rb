772d85755b0818561b1ab90e26c87e27a2db611b
Henning Koch <henning.koch@makandra.de>
2010-06-23 19:11:56 +0200

    prevented class reloading from removing the diet

--- lib/query_diet/action_controller_ext.rb
+++ lib/query_diet/action_controller_ext.rb
@@ -1,15 +1,20 @@
-require_dependency 'application_controller'
+# require_dependency 'application_controller'
 
-ApplicationController.class_eval do
+already_inherited = defined?(ApplicationController)
 
-  around_filter :query_diet_logging
+[ActionController::Base, already_inherited ? ApplicationController : nil].compact.each do |base|
+  base.class_eval do
 
-  private
+    around_filter :query_diet_logging
 
-  def query_diet_logging
-    QueryDiet::Logger.reset
-    yield
-    QueryDiet::Widget.render(response)
+    private
+
+    def query_diet_logging
+      QueryDiet::Logger.reset
+      yield
+      QueryDiet::Widget.render(response)
+    end
+
   end
 
 end
