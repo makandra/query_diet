4c865ed01f21ae8e71fcd82718a428693a26ead3
Andreas Robecke <andreas.robecke@makandra.de>
2013-06-26 17:50:01 +0200

    wip

--- lib/query_diet/visualizer.rb
+++ lib/query_diet/visualizer.rb
@@ -1,23 +1,22 @@
 module QueryDiet
   module Widget
     class Visualizer
-        
+
       DEFAULT_FONT_SIZE = 16
-      MAX_FONT_SIZE = 120  
-        
+
       def initialize(options = {})
-        @bad_count = options[:bad_count] || 8
-        @bad_time = options[:bad_time] || 5000
+        @base_queries = options[:base_queries] || 8
+        @growth_per_query = options[:growth_per_query] || 2
       end
-      
+
       def html
         <<-EOF
         <div id="query_diet" class="#{bad? ? 'bad' : 'good' }" onclick="this.parentNode.removeChild(this);">
-          #{count} / #{time}ms
+          #{count} #{count == 1 ? 'query' : 'queries'}
         </div>
         EOF
       end
-      
+
       def css
         <<-EOF
         <style type="text/css"><!--
@@ -43,63 +42,32 @@ module QueryDiet
           }
           div#query_diet.bad {
             background-color: red;
-            font-size: #{font_size}px;
-            line-height: #{font_size}px;
+            font-size: #{font_size_by_query_count}px;
+            line-height: #{font_size_by_query_count}px;
           }
         --></style>
         EOF
       end
-      
+
       private
-       
+
       def bad?
-        count >= @bad_count or time >= (@bad_time/1000)
+        count > @base_queries
       end
-      
-      def count 
+
+      def count
         QueryDiet::Logger.count
       end
-      
-      def time
-        QueryDiet::Logger.time
-      end
-      
-      def font_size
-        calculated_font_size = [font_size_by_query_count, font_size_by_query_time].max
-        normalised_font_size = [calculated_font_size, MAX_FONT_SIZE].min
-        @font_size ||= normalised_font_size
-      end
-      
+
       def font_size_by_query_count
-        overrun = count - @bad_count
+        overrun = count - @base_queries
         if overrun > 0
-          overrun_degree = fibonaci(overrun) 
-          (((overrun_degree.to_f/@bad_count.to_f) * DEFAULT_FONT_SIZE) + DEFAULT_FONT_SIZE).to_i
+          DEFAULT_FONT_SIZE + @growth_per_query * overrun
         else
-          DEFAULT_FONT_SIZE  
+          DEFAULT_FONT_SIZE
         end
       end
-      
-      def font_size_by_query_time
-        overrun = time - (@bad_time / 1000)
-        if overrun > 0
-          overrun_degree = fibonaci(overrun)
-          (((overrun_degree.to_f/(@bad_time/1000).to_f) * DEFAULT_FONT_SIZE) + DEFAULT_FONT_SIZE).to_i
-        else
-          DEFAULT_FONT_SIZE  
-        end
-      end
-      
-      def fibonaci(upto)
-        x1 = x2 = fx = 1
-        upto.times do
-          fx = x1 + x2
-          x1 = x2
-          x2 = fx  
-        end
-        fx  
-      end
-      
+
     end
-  end 
+  end
 end
--- spec/shared/query_diet/integration/widget_spec.rb
+++ spec/shared/query_diet/integration/widget_spec.rb
@@ -9,15 +9,15 @@ describe 'The widget', :type => defined?(RSpec) ? :request : :integration do
 
   it 'should show the correct number of queries' do
     get 'query_diet/no_query'
-    response.should have_selector('#query_diet', :content => '0 /')
+    response.should have_selector('#query_diet', :content => '0 queries')
     get 'query_diet/two_queries'
-    response.should have_selector('#query_diet', :content => '2 /')
+    response.should have_selector('#query_diet', :content => '2 queries')
   end
 
   it 'should reset the queries after each request' do
     get 'query_diet/two_queries'
     get 'query_diet/two_queries'
-    response.should have_selector('#query_diet', :content => '2 /')
+    response.should have_selector('#query_diet', :content => '2 queries')
   end
 
 end
