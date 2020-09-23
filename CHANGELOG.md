f018a3adc8ad4df0f1d1589222db405c0c655134
Johanna Hartmann <johanna.hartmann@makandra.de>
2020-09-23 15:42:57 +0200

    Allow to pass in a nonce to fulfill the CSP in development (closes #23)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ This project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html
 
 
 ## Unreleased
+### Compatible changes
+### Breaking changes
+
+## 0.7.0 2020-09-24
+### Compatible changes
+
+* Add #23: Allow to pass in a nonce to fulfill the CSP in development. It is used as follows:
+  ```
+  = query_diet_widget(nonce: true) if Rails.env.development?
+  ```
 
 ### Compatible changes
+
 - This CHANGELOG file.
--- lib/query_diet/widget.rb
+++ lib/query_diet/widget.rb
@@ -2,9 +2,9 @@ module QueryDiet
   module Widget
     class << self
 
-      def css
+      def css(nonce_attribute)
         <<-EOF
-        <style type="text/css"><!--
+        <style type="text/css"#{nonce_attribute}><!--
           div#query_diet {
             position: absolute;
             top: 0;
@@ -31,9 +31,19 @@ module QueryDiet
         EOF
       end
 
+      def js(nonce_attribute)
+        <<-EOF
+        <script type="text/javascript"#{nonce_attribute}>
+          document.getElementById("query_diet").addEventListener("click", function() {
+            this.parentNode.removeChild(this);
+          });
+        </script>
+        EOF
+      end
+
       def html(options)
         <<-EOF
-        <div id="query_diet" class="#{QueryDiet::Logger.bad?(options) ? 'bad' : 'good' }" onclick="this.parentNode.removeChild(this);">
+        <div id="query_diet" class="#{QueryDiet::Logger.bad?(options) ? 'bad' : 'good' }">
           #{QueryDiet::Logger.count} / #{QueryDiet::Logger.time}ms
         </div>
         EOF
@@ -42,8 +52,12 @@ module QueryDiet
     end
 
     module Helper
-      def query_diet_widget(options = {})
-        html = Widget.css + Widget.html(options)
+      def query_diet_widget(options = { 'nonce' => false })
+        with_nonce = options.fetch('nonce', options[:nonce])
+        nonce = with_nonce ? content_security_policy_nonce : nil
+        nonce_attribute = nonce ? " nonce=\"#{nonce}\"" : ''
+
+        html = Widget.css(nonce_attribute) + Widget.html(options) + Widget.js(nonce_attribute)
         html.respond_to?(:html_safe) ? html.html_safe : html
       end
     end
