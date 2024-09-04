63d0829a3e1fefe7feca4ba203a7130a7eaf5d55
Florian Leinsinger <florian.leinsinger@makandra.de>
2024-09-04 16:06:54 +0200

    Rename test application

--- spec/rails-5.2/config/routes.rb
+++ spec/rails-5.2/config/routes.rb
@@ -1,4 +1,4 @@
-HasDefaultSpecApp::Application.routes.draw do
+QueryDietSpecApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/rails-6.1/config/environment.rb
+++ spec/rails-6.1/config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-HasDefaultSpecApp::Application.initialize!
+QueryDietSpecApp::Application.initialize!
